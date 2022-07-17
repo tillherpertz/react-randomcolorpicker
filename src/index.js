d868ada8b7ef7a82380db01e4f8a530eb80e4fc2
tillherpertz <tillh@live.de>
2022-07-17 23:34:47 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Button} from './button';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Random extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { color: [255, 186, 3]}
+	this.handleClick = this.handleClick.bind(this)
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  componentDidMount() {
+    this.applyColor();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.applyColor();
+  }
+
+  formatColor(ary) {
+    return 'rgb(' + ary.join(', ') + ')';
+  }
+
+  isLight() {
+    const rgb = this.state.color;
+    return rgb.reduce((a,b) => a+b) < 127 * 3;
+  }
+
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.background = color;
+  }
+
+  chooseColor() {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random()*256));
+    }
+    return random;
+  }
+
+  handleClick() {
+	this.setState({
+		color: this.chooseColor()
+	});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className={this.isLight() ? 'white' : 'black'}>
+          Your color is {this.formatColor(this.state.color)}.
+        </h1>
+		<Button onClick={this.handleClick}></Button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+	<Random />, 
+	document.getElementById('root')
+  );
